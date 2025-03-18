6efde634018ba382a11e3e27d21db71f7d133f9c
Gurnoorkamboj <gurnoorkombaj@gmail.com>
2025-03-18 14:47:49 +0530

    Solution to Prime Number Checker in a Specific Range

--- Prime Number Checker in a Specific Range@@67ca7acd1fe08d7207c253b6/code.c
+++ Prime Number Checker in a Specific Range@@67ca7acd1fe08d7207c253b6/code.c
@@ -1,6 +1,6 @@
 int isPrime(int num){
-    for (int i=1;i<=num;i++){
-        for (int j=1;j<=i;j++){
+    for (int i=1;i<num;i++){
+        for (int j=1;j<i;j++){
             if (j%i==0)
                 return 1;    
         }
